4d378c202dc51ecca8ec02667cf40a68dc495766
Markow Kirill <69markuss11@gmail.com>
2024-11-27 21:05:04 +0200

    inf

--- DrugStoreBot/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ DrugStoreBot/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe6a1c4fa3e5ea45f3b72a7cdcdfd6cf12a4e5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d77a6572a5dada3713368e37afbf232a4eb5bccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DrugStoreBot/DomainTests/obj/Debug/net8.0/DomainTests.AssemblyInfo.cs
+++ DrugStoreBot/DomainTests/obj/Debug/net8.0/DomainTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DomainTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe6a1c4fa3e5ea45f3b72a7cdcdfd6cf12a4e5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d77a6572a5dada3713368e37afbf232a4eb5bccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DomainTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DomainTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
